46bb9d1938de1e1e1e1c1b47dd56d333d04820da
Mook <natthaporn.wim@dome.tu.ac.th>
2021-10-13 21:30:33 +0700

    Initial Commit

---
+++ Instagram-Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagram-Clone
+//
+//  Created by Natthaporn Wimonanupong on 13/10/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instagram-Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram-Clone
+//
+//  Created by Natthaporn Wimonanupong on 13/10/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
